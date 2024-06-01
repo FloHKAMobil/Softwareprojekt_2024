7639f5836427700810b231a7539d167137caa69c
FloHKAMobil <flo.taubenberger@web.de>
2024-06-01 15:48:17 +0200

    CSV-Parser für Erhebungsstand und Zaehlfahrten, Datei muss aktuell in bestimmten Pfad liegen!!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,27 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Erhebungsstand_CsvParser.main(args);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }*/
+
+        try {
+            Zaehlfahrten_CsvParser.main(args);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
-
-// HALLOOO TEEEEEEEEEEEEEEEEESSSSSSSSSSSSSSSTTTT
-//teeeestttttt (von Tom)
-//Testeteststs
-
-// Hhuu, ich bin auch da
