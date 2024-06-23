50db2cd11226f346895987dafb76604291217994
FloHKAMobil <flo.taubenberger@web.de>
2024-06-23 18:32:26 +0200

    Dispoliste - Tagesgruppe (4): -Neuer Speicherort -zusätzliche Logdateien für Fahrtverteilung, Linien, und Tagesgruppe

--- src/main/java/org/example/Evaluation.java
+++ src/main/java/org/example/Evaluation.java
@@ -5,6 +5,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,7 +21,6 @@ public class Evaluation {
     //Bewertung des Erhebungsstandes nach Tagesgruppe und Linie (!!!Fahrt aktuell außenvorgelassen weil eig. schon in CSV-Datei beschrieben!!!)
     public static void main(String[] args) {
         String csvFile = Config.getCsvFilePathErhebungsstand();
-        //String csvFile = "E:\\Eigene Dateien\\Daten Florian\\Studium\\4.Semester\\VSMB430 Softwareentwicklung\\Projekt\\Erhebungsstand.csv";
         Map<String, FahrtDaten> tagesgruppenMap = new HashMap<>();
         Map<String, FahrtDaten> linienMap = new HashMap<>();
 
@@ -61,45 +61,50 @@ public class Evaluation {
             e.printStackTrace();
         }
 
-        /*System.out.println(" ");
+        String userName = System.getProperty("user.name");
+        String filePath2 = "C:/Users/" + userName + "/AppData/Roaming/Dispositionssoftware/tagesgruppelist_log.txt";
+        String filePath3 = "C:/Users/" + userName + "/AppData/Roaming/Dispositionssoftware/linienlist_log.txt";
+        String filePath4 = "C:/Users/" + userName + "/AppData/Roaming/Dispositionssoftware/fahrtverteilunglist_log.txt";
+
+        writeToFile(filePath2, tagesgruppenMap, "Tagesgruppe");
+        writeToFile(filePath3, linienMap, "Linie");
+
         System.out.println(" ");
-        System.out.println("Nach Tagesgruppe:");*/
+        System.out.println(" ");
+        System.out.println("Nach Tagesgruppe:");
         for (Map.Entry<String, FahrtDaten> entry : tagesgruppenMap.entrySet()) {
             String tagesgruppe = entry.getKey();
             FahrtDaten daten = entry.getValue();
-            /*System.out.println("Tagesgruppe: " + tagesgruppe);
+            System.out.println("Tagesgruppe: " + tagesgruppe);
             System.out.println("Geplante Fahrten: " + daten.geplanteFahrten);
             System.out.println("Erhobene Fahrten: " + daten.erhobeneFahrten);
             System.out.println("Güteprüfung ok: " + daten.guetepruefungOk);
-            System.out.println();*/
-        }
-
-        /*System.out.println("Nach Linie:");*/
-        for (Map.Entry<String, FahrtDaten> entry : linienMap.entrySet()) {
-            String linie = entry.getKey();
-            FahrtDaten daten = entry.getValue();
-            /*System.out.println("Linie: " + linie);
-            System.out.println("Geplante Fahrten: " + daten.geplanteFahrten);
-            System.out.println("Erhobene Fahrten: " + daten.erhobeneFahrten);
-            System.out.println("Güteprüfung ok: " + daten.guetepruefungOk);*/
             System.out.println();
         }
 
+        System.out.println("Nach Linie:");
+        for (Map.Entry<String, FahrtDaten> entry : linienMap.entrySet()) {
+            String linie = entry.getKey();
+            FahrtDaten daten = entry.getValue();
+            System.out.println("Linie: " + linie);
+            System.out.println("Geplante Fahrten: " + daten.geplanteFahrten);
+            System.out.println("Erhobene Fahrten: " + daten.erhobeneFahrten);
+            System.out.println("Güteprüfung ok: " + daten.guetepruefungOk);
+            System.out.println();
+        }
 
-        //Bewertung der Fahrtverteilung über ein Quartal
-        /*System.out.println(" ");
+        // Bewertung der Fahrtverteilung über ein Quartal
         System.out.println(" ");
-        System.out.println("Fahrtverteilung:");*/
+        System.out.println(" ");
+        System.out.println("Fahrtverteilung:");
         String erhebungsstandCsvFile = Config.getCsvFilePathErhebungsstand();
         String zaehlfahrtenCsvFile = Config.getCsvFilePathZaehlfahrten();
-        //String erhebungsstandCsvFile = "E:\\Eigene Dateien\\Daten Florian\\Studium\\4.Semester\\VSMB430 Softwareentwicklung\\Projekt\\Erhebungsstand.csv";
-        //String zaehlfahrtenCsvFile = "E:\\Eigene Dateien\\Daten Florian\\Studium\\4.Semester\\VSMB430 Softwareentwicklung\\Projekt\\Zaehlfahrten.csv";
         List<FahrtDaten.Fahrt> fahrtenListe = new ArrayList<>();
 
         // Mapping von Tagesgruppen
         Map<String, String> tagesgruppenMap2 = new HashMap<>();
         tagesgruppenMap2.put("Schule", "Montag - Freitag Schule");
-        tagesgruppenMap2.put("Ferien inkl. Br�ckentag", "Montag - Freitag Ferien");
+        tagesgruppenMap2.put("Ferien inkl. Brückentag", "Montag - Freitag Ferien");
         tagesgruppenMap2.put("Sonntag", "Sonn-/Feiertag");
         tagesgruppenMap2.put("Feiertag", "Sonn-/Feiertag");
 
@@ -150,45 +155,34 @@ public class Evaluation {
 
             for (Zaehlfahrten fahrt : zaehlfahrten) {
                 String mappedTagesgruppe = tagesgruppenMap2.getOrDefault(fahrt.getTagesgruppe(), fahrt.getTagesgruppe());
-
-
                 String zaehlAbfahrtszeit = fahrt.getAbfahrtszeit().substring(0, 5); // Extrahiere nur die ersten 5 Zeichen (HH:mm)
 
-
-
-
                 for (FahrtDaten.Fahrt f : fahrtenListe) {
-
-
                     if (f.getLinie().equals(fahrt.getLinie())
                             && f.getRichtung().equals(fahrt.getRichtung())
                             && f.getTagesgruppe().equals(mappedTagesgruppe)
                             && f.getStarthaltestelle().equals(fahrt.getStarthaltestelle())
                             && f.getAbfahrtszeit().equals(zaehlAbfahrtszeit)) {
                         f.addDatum(fahrt.getDatum());
-                    } /*else {
-                        System.out.println("No match for:");
-                        System.out.println("Zaehlfahrten Linie: " + fahrt.getLinie() + ", Richtung: " + fahrt.getRichtung() +
-                                ", Tagesgruppe: " + mappedTagesgruppe + ", Starthaltestelle: " + fahrt.getStarthaltestelle() +
-                                ", Abfahrtszeit: " + zaehlAbfahrtszeit);
-                        System.out.println("Erhebungsstand Linie: " + f.getLinie() + ", Richtung: " + f.getRichtung() +
-                                ", Tagesgruppe: " + f.getTagesgruppe() + ", Starthaltestelle: " + f.getStarthaltestelle() +
-                                ", Abfahrtszeit: " + f.getAbfahrtszeit());
-                    }*/
+                    }
                 }
             }
 
-            /*for (FahrtDaten.Fahrt f : fahrtenListe) {
+            // Schreiben der Fahrtverteilung in die Datei
+            writeFahrtverteilungToFile(filePath4, fahrtenListe);
+
+            // Optional: Ausgabe der Fahrtverteilung auf der Konsole
+            for (FahrtDaten.Fahrt f : fahrtenListe) {
                 System.out.println("Linie: " + f.getLinie());
                 System.out.println("Richtung: " + f.getRichtung());
                 System.out.println("Tagesgruppe: " + f.getTagesgruppe());
                 System.out.println("Starthaltestelle: " + f.getStarthaltestelle());
                 System.out.println("Abfahrtszeit: " + f.getAbfahrtszeit());
                 System.out.println("Anzahl geplanter Fahrten: " + f.getGeplanteFahrten());
-                System.out.println("Anzahl Fahrten mit erfolgreicher Gueteprüfung: " + f.getGuetepruefungOk());
+                System.out.println("Anzahl Fahrten mit erfolgreicher Güteprüfung: " + f.getGuetepruefungOk());
                 System.out.println("Daten: " + String.join(", ", f.getDaten()));
                 System.out.println();
-            }*/
+            }
 
             // Abspeichern der Fahrtenliste
             for (FahrtDaten.Fahrt f : fahrtenListe) {
@@ -196,9 +190,58 @@ public class Evaluation {
                 fahrtenMap.computeIfAbsent(key, k -> new ArrayList<>()).add(f);
             }
 
-
-
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeToFile(String filePath, Map<String, FahrtDaten> dataMap, String type) {
+        try (FileWriter writer = new FileWriter(filePath, false)) { // 'false' means overwrite mode
+            for (Map.Entry<String, FahrtDaten> entry : dataMap.entrySet()) {
+                String key = entry.getKey();
+                FahrtDaten daten = entry.getValue();
+                String output = String.format(
+                        "%s: %s\nGeplante Fahrten: %d\nErhobene Fahrten: %d\nGüteprüfung ok: %d\n\n",
+                        type, key, daten.geplanteFahrten, daten.erhobeneFahrten, daten.guetepruefungOk
+                );
+
+                // Print to console
+                System.out.print(output);
+
+                // Write to file
+                writer.write(output);
+            }
+            System.out.println("Erfolgreich in die Datei geschrieben: '" + filePath + "'");
+        } catch (IOException e) {
+            System.out.println("Ein Fehler ist beim Schreiben in die Datei aufgetreten.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeFahrtverteilungToFile(String filePath, List<FahrtDaten.Fahrt> fahrtenListe) {
+        try (FileWriter writer = new FileWriter(filePath, false)) { // 'false' means overwrite mode
+            for (FahrtDaten.Fahrt f : fahrtenListe) {
+                String output = String.format(
+                        "Linie: %s\nRichtung: %s\nTagesgruppe: %s\nStarthaltestelle: %s\nAbfahrtszeit: %s\nAnzahl geplanter Fahrten: %d\nAnzahl Fahrten mit erfolgreicher Güteprüfung: %d\nDaten: %s\n\n",
+                        f.getLinie(),
+                        f.getRichtung(),
+                        f.getTagesgruppe(),
+                        f.getStarthaltestelle(),
+                        f.getAbfahrtszeit(),
+                        f.getGeplanteFahrten(),
+                        f.getGuetepruefungOk(),
+                        String.join(", ", f.getDaten())
+                );
+
+                // Print to console
+                System.out.print(output);
+
+                // Write to file
+                writer.write(output);
+            }
+            System.out.println("Erfolgreich in die Datei geschrieben: '" + filePath + "'");
+        } catch (IOException e) {
+            System.out.println("Ein Fehler ist beim Schreiben in die Datei aufgetreten.");
             e.printStackTrace();
         }
     }
